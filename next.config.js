c7ea0190621739d4ecefef07f7a0152efe5721b7
Jmr Sai <jmrsai987@gmail.com>
2025-10-19 14:54:46 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.js
+++ next.config.js
@@ -17,6 +17,11 @@ const nextConfig = {
       },
     ],
   },
+  experimental: {
+    // This allows the Next.js dev server to accept requests from any origin.
+    // This is required for the development environment to work correctly.
+    allowedNextBundlerReactRoots: ['.'],
+  }
 };
 
 module.exports = nextConfig;
