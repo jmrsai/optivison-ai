00fa0b65786b005c3a4eee4b8e5ea279e70989bb
Jmr Sai <jmrsai987@gmail.com>
2025-10-22 14:56:07 +0000

    add firebase webpack

--- next.config.js
+++ next.config.js
@@ -21,7 +21,14 @@ const nextConfig = {
   // This is required for the development environment to work correctly.
   experimental: {
     // Empty as allowedNextBundlerReactRoots has been moved.
-  }
+  },
+  webpack: (config, { isServer }) => {
+    // Exclude firebase-admin from client-side bundles
+    if (!isServer) {
+      config.externals.push('firebase-admin');
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
