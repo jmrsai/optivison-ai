b1617727403694ae8280ce6149a27fb969542e5f
Jmr Sai <jmrsai987@gmail.com>
2025-10-22 16:32:46 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -2,6 +2,7 @@
 import { getApps, initializeApp, type FirebaseApp } from 'firebase/app';
 import { getAuth, type Auth } from 'firebase/auth';
 import { getFirestore, type Firestore } from 'firebase/firestore';
+import { getAnalytics, type Analytics } from "firebase/analytics";
 
 import { getFirebaseConfigClient } from './config';
 
@@ -9,6 +10,7 @@ export function initializeFirebase(): {
   firebaseApp: FirebaseApp;
   firestore: Firestore;
   auth: Auth;
+  analytics: Analytics | null;
 } {
   const apps = getApps();
   const app = apps.length > 0 ? apps[0] : initializeApp(getFirebaseConfigClient());
@@ -16,7 +18,12 @@ export function initializeFirebase(): {
   const auth = getAuth(app);
   const firestore = getFirestore(app);
   
-  return { firebaseApp: app, firestore, auth };
+  let analytics: Analytics | null = null;
+  if (typeof window !== 'undefined') {
+    analytics = getAnalytics(app);
+  }
+
+  return { firebaseApp: app, firestore, auth, analytics };
 }
 
 export * from './provider';
